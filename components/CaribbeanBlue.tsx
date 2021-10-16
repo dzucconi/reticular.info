115690c0053e85868c0f245703ab4b4a4b97b1d0
dzucconi <mail@damonzucconi.com>
2021-10-16 16:14:04 -0400

    Adjustments

---
+++ components/CaribbeanBlue.tsx
@@ -0,0 +1,195 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { sample } from "../util/sample";
+
+export const CaribbeanBlue: React.FC<{ speed: number }> = ({
+  speed,
+  ...rest
+}) => {
+  const [colors, setColors] = useState(sample(COLORS, 7));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setColors((prevColors) => {
+        const [_head, ...tail] = prevColors;
+        return [...tail, sample(COLORS)];
+      });
+    }, speed);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [speed]);
+
+  return (
+    <Colors {...rest}>
+      {colors.map((color, i) => {
+        return (
+          <Color key={color + i} style={{ color }}>
+            {color}
+          </Color>
+        );
+      })}
+    </Colors>
+  );
+};
+
+const Colors = styled.div`
+  position: relative;
+  padding: 1rem;
+
+  &:after {
+    content: "x";
+    font-size: 5vw;
+    color: transparent;
+  }
+`;
+
+const Color = styled.div`
+  font-size: 5vw;
+  position: absolute;
+  mix-blend-mode: multiply;
+`;
+
+const COLORS = [
+  "aliceblue",
+  "antiquewhite",
+  "aqua",
+  "aquamarine",
+  "azure",
+  "beige",
+  "bisque",
+  "black",
+  "blanchedalmond",
+  "blue",
+  "blueviolet",
+  "brown",
+  "burlywood",
+  "cadetblue",
+  "chartreuse",
+  "chocolate",
+  "coral",
+  "cornflowerblue",
+  "cornsilk",
+  "crimson",
+  "cyan",
+  "darkblue",
+  "darkcyan",
+  "darkgoldenrod",
+  "darkgray",
+  "darkgreen",
+  "darkkhaki",
+  "darkmagenta",
+  "darkolivegreen",
+  "darkorange",
+  "darkorchid",
+  "darkred",
+  "darksalmon",
+  "darkseagreen",
+  "darkslateblue",
+  "darkslategray",
+  "darkturquoise",
+  "darkviolet",
+  "deeppink",
+  "deepskyblue",
+  "dimgray",
+  "dodgerblue",
+  "firebrick",
+  "floralwhite",
+  "forestgreen",
+  "fuchsia",
+  "gainsboro",
+  "ghostwhite",
+  "gold",
+  "goldenrod",
+  "gray",
+  "green",
+  "greenyellow",
+  "honeydew",
+  "hotpink",
+  "indianred",
+  "indigo",
+  "ivory",
+  "khaki",
+  "lavender",
+  "lavenderblush",
+  "lawngreen",
+  "lemonchiffon",
+  "lightblue",
+  "lightcoral",
+  "lightcyan",
+  "lightgoldenrodyellow",
+  "lightgray",
+  "lightgreen",
+  "lightpink",
+  "lightsalmon",
+  "lightseagreen",
+  "lightskyblue",
+  "lightslategray",
+  "lightsteelblue",
+  "lightyellow",
+  "lime",
+  "limegreen",
+  "linen",
+  "magenta",
+  "maroon",
+  "mediumaquamarine",
+  "mediumblue",
+  "mediumorchid",
+  "mediumpurple",
+  "mediumseagreen",
+  "mediumslateblue",
+  "mediumspringgreen",
+  "mediumturquoise",
+  "mediumvioletred",
+  "midnightblue",
+  "mintcream",
+  "mistyrose",
+  "moccasin",
+  "navajowhite",
+  "navy",
+  "oldlace",
+  "olive",
+  "olivedrab",
+  "orange",
+  "orangered",
+  "orchid",
+  "palegoldenrod",
+  "palegreen",
+  "paleturquoise",
+  "palevioletred",
+  "papayawhip",
+  "peachpuff",
+  "peru",
+  "pink",
+  "plum",
+  "powderblue",
+  "purple",
+  "red",
+  "rosybrown",
+  "royalblue",
+  "saddlebrown",
+  "salmon",
+  "sandybrown",
+  "seagreen",
+  "seashell",
+  "sienna",
+  "silver",
+  "skyblue",
+  "slateblue",
+  "slategray",
+  "snow",
+  "springgreen",
+  "steelblue",
+  "tan",
+  "teal",
+  "thistle",
+  "tomato",
+  "turquoise",
+  "violet",
+  "wheat",
+  "white",
+  "whitesmoke",
+  "yellow",
+  "yellowgreen",
+];
