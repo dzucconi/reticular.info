e2d0b06c0760c217e73f8064a621d184bd8d9bd7
dzucconi <mail@damonzucconi.com>
2021-10-17 18:15:00 -0400

    Aligns editor styling

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import styled from "styled-components";
 
 interface EditorProps {
@@ -14,46 +14,69 @@ export const Editor: React.FC<EditorProps> = ({ changeValue = IDENTITY }) => {
     setValue(event.target.value);
   };
 
+  const handleFocus = () => {
+    inputRef.current?.focus();
+  };
+
   const changedValue = useMemo(() => changeValue(value), [changeValue, value]);
 
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
   return (
-    <Container>
+    <>
       <Input
+        ref={inputRef as any}
         autoFocus
         value={value}
         onChange={handleChange}
         placeholder="Begin typing"
       />
-      <Output readOnly value={changedValue} />
-    </Container>
+
+      <Output
+        readOnly
+        value={changedValue}
+        placeholder="Begin typing"
+        onFocus={handleFocus}
+      />
+    </>
   );
 };
 
-const Container = styled.div`
-  display: flex;
+const Textarea = styled.textarea`
   position: fixed;
   top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
-
-const Textarea = styled.textarea`
+  bottom: 0;
   appearance: none;
   flex: 1;
   height: 100%;
   border-radius: 0%;
   border: 0;
   padding: 1rem;
-  font-size: 2rem;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 const Input = styled(Textarea)`
   color: black;
   background-color: white;
+  right: 0;
+  font-size: 1rem;
+  width: 33.33%;
+
+  &:hover {
+    z-index: 1;
+  }
 `;
 
 const Output = styled(Textarea)`
   color: white;
   background-color: black;
+  width: 90%;
+  left: 0;
+  background-color: black;
+  color: white;
+  font-size: 3rem;
+  padding: 1em;
 `;
